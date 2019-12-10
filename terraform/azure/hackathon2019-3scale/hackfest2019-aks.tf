030c773932196314f6b81c039dee9e27402d7380
Rael Garcia <rael@redhat.com>
2019-12-10 16:10:43 +0100

    Add azure dns zone and hackfest record

--- terraform/azure/hackathon2019-3scale/hackfest2019-aks.tf
+++ terraform/azure/hackathon2019-3scale/hackfest2019-aks.tf
@@ -9,7 +9,26 @@ module "aks" {
   name           = "hackfest2019"
 }
 
+resource "azurerm_dns_zone" "az_mc" {
+  name                = "az.mc.3sca.net"
+  resource_group_name = "${azurerm_resource_group.aks.name}"
+}
+
+resource "azurerm_dns_a_record" "az_mc_wildcard" {
+  name                = "*"
+  zone_name           = "${azurerm_dns_zone.az_mc.name}"
+  resource_group_name = "${azurerm_resource_group.aks.name}"
+  ttl                 = 300
+  records             = ["52.166.37.118"] # aks traefik load balancer ip
+
+}
+
 output "az-cluster-login" {
   description = "AZ CLI command to get cluster credentiales and set kubectl"
   value       = "az aks get-credentials --resource-group ${azurerm_resource_group.aks.name} --name ${module.aks.cluster_name}"
+}
+
+output "az-dns-zone" {
+  description = "Azure Hackathon2019 dns zone"
+  value       = "${azurerm_dns_zone.az_mc.name_servers}"
 }
